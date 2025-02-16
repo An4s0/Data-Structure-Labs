14d9a23eea159522ccf1478102ec20674f3714ac
Anas Almutary <me@ianas.me>
2025-02-16 21:02:04 +0300

    final commit

--- Lab3/Task1/src/App.java
+++ Lab3/Task1/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+
+        list.addLast("B");
+        list.addLast("C");
+        list.addFirst("A");
+        list.addNode("D", 3);
+        list.removeFirst();
+        list.findNode("A");
+        list.removeNode(2);
+        list.findNode("C");
+
+        System.out.println(list.toString());
     }
 }
--- Lab3/Task1/src/SinglyLinkedList.java
+++ Lab3/Task1/src/SinglyLinkedList.java
@@ -38,21 +38,24 @@ public class SinglyLinkedList<E> {
     }
 
     public void addNode(E data, int position) {
-        Node<E> newNode = new Node<E>(data);
+        if (position < 0 || position > size) {
+            System.out.println("Invalid position");
+            return;
+        }
+
         if (position == 0) {
             addFirst(data);
         } else if (position == size) {
             addLast(data);
         } else if (position > 0 && position < size) {
+            Node<E> newNode = new Node<E>(data);
             Node<E> current = head;
-            for (int i = 1; i < position; i++) {
+            for (int i = 0; i < position - 1; i++) {
                 current = current.getNext();
             }
             newNode.setNext(current.getNext());
             current.setNext(newNode);
             size++;
-        } else {
-            System.out.println("Invalid position");
         }
     }
 
@@ -73,11 +76,51 @@ public class SinglyLinkedList<E> {
         int count = 0;
         while (current != null) {
             if (current.getData().equals(data)) {
-                System.out.println("Value found at Node" + count);
+                System.out.println("Value found at Node: " + count);
                 return;
             }
+            current = current.getNext();
             count++;
         }
+        System.out.println("Value not found");
+    }
+
+    public void removeNode(int position) {
+        if (position < 0 || position >= size) {
+            System.out.println("Invalid position");
+            return;
+        }
+
+        if (position == 0) {
+            removeFirst();
+        } else if (position == (size - 1)) {
+            Node<E> current = head;
+            for (int i = 0; i < size - 2; i++) {
+                current = current.getNext();
+            }
+            current.setNext(null);
+            tail = current;
+            size--;
+        } else {
+            Node<E> current = head;
+            for (int i = 0; i < position; i++) {
+                current = current.getNext();
+            }
+            current.setNext(current.getNext().getNext());
+            size--;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        String output = "";
+        Node<E> current = head;
+        while (current != null) {
+            output += current.getData() + " ";
+            current = current.getNext();
+        }
+        return output;
     }
 
 }
