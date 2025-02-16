e94789a8ff82ded064764ac187ab0ea2ed399812
Anas Almutary <me@ianas.me>
2025-02-16 15:33:04 +0300

    add: addFirst()

---
+++ Lab3/Task1/src/SinglyLinkedList.java
@@ -0,0 +1,26 @@
+public class SinglyLinkedList<E> {
+    public Node<E> head;
+    public Node<E> tail;
+    public int size;
+
+    public SinglyLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void addFirst(E data){
+        Node<E> newNode = new Node<E>(data);
+        if(isEmpty()){
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.setNext(head);
+            head = newNode;
+        }
+    }
+}
